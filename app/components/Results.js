aadbe3fe49487828232b8f9adb23192410bd7b40
Silvi47 <80204242+Silvi47@users.noreply.github.com>
2022-03-07 22:09:55 +0700

    Update Results.js

--- app/components/Results.js
+++ app/components/Results.js
@@ -103,11 +103,6 @@ export default class Results extends React.Component {
             <ProfileList profile={loser.profile}/>
           </Card>
         </div>
-        <button
-          onClick={this.props.onReset}
-          className='btn dark-btn btn-space'>
-            Reset
-        </button>
       </React.Fragment>
     )
   }
