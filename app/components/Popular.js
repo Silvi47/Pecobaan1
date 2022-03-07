3c721b3f0627999a9de4adce3c627e52f23d8950
Silvi47 <80204242+Silvi47@users.noreply.github.com>
2022-03-07 21:38:00 +0700

    Update Popular.js

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -59,7 +59,7 @@ function ReposGrid ({ repos }) {
                                 </li>
                                 <li>
                                     <FaExclamationTriangle color="rgb(241, 138, 147)" size={22} />
-                                    {open_issues.toLocaleString()} open open_issues
+                                    {open_issues.toLocaleString()} open_issues
                                 </li>
                             </ul>
                         </Card>
