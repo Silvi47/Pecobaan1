8bc6392aba4fb6375ef0f5aa43ec265aeb82a924
Silvi47 <80204242+Silvi47@users.noreply.github.com>
2022-03-09 22:30:15 +0700

    Update index.js

--- app/index.js
+++ app/index.js
@@ -32,12 +32,12 @@ class App extends React.Component {
         <ThemeProvider value={this.state}>
           <div className={this.state.theme}>
             <div>
-              <ul className="flex-center">
+              <ul className={`flex-center ${this.state.theme}`}>
                 <li>
-                  <Link to="/" className={`btn-clear nav-link ${this.state.theme}`}>Battle</Link>
+                  <Link to="/" className={`btn-clear nav-link`}>Battle</Link>
                 </li>
                 <li>
-                  <Link to="/popular" className={`btn-clear nav-link ${this.state.theme}`}>Popular</Link>
+                  <Link to="/popular" className={`btn-clear nav-link`}>Popular</Link>
                 </li>
                 <li>
                   <Nav />
