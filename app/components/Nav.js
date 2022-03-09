85f844151b29d273444402131c36da8121c1610a
Silvi47 <80204242+Silvi47@users.noreply.github.com>
2022-03-09 19:35:47 +0700

    Create Nav.js

---
+++ app/components/Nav.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { ThemeConsumer  } from "../contexts/theme";
+
+function Nav () {
+    return (
+        <ThemeConsumer>
+            {({ theme, toggleTheme }) => (
+                <nav className="row space-between">
+                    <button
+                        className="btn-clear nav-link"
+                        onClick={toggleTheme}
+                    >
+                        {theme === 'light' ? 'Dark Theme' : <div style={{color: "white"}}>Light Theme</div>}
+                    </button>
+                </nav>
+            )}
+        </ThemeConsumer>
+    )
+}
+
+export default Nav;
