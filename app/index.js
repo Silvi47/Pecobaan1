b0f580a55f6343bf44a4ace61eeb86bd3bfe6e4c
Silvi47 <80204242+Silvi47@users.noreply.github.com>
2022-03-10 21:06:08 +0700

    Update index.js

--- app/index.js
+++ app/index.js
@@ -33,8 +33,8 @@ class App extends React.Component {
             <div className='container'>
               <Nav />
               <Routes>
-                <Route exact path="/" element={<Battle />} />
-                <Route exact path="/battle" element={<Popular />}/>
+                <Route exact path="/" element={<Popular />} />
+                <Route exact path="/battle" element={<Battle />}/>
               </Routes>
             </div>
           </div>
