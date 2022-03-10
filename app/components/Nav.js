6a65fab5498d26072dc56a931dba18d184c95406
Silvi47 <80204242+Silvi47@users.noreply.github.com>
2022-03-10 20:36:30 +0700

    Update Nav.js

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { ThemeConsumer  } from "../contexts/theme";
-import Popular from './Popular'
-import Battle from './Battle'
 import {
   BrowserRouter as Router,
   Routes,
@@ -13,28 +11,22 @@ function Nav () {
     return (
         <ThemeConsumer>
             {({ theme, toggleTheme }) => (
-              <Router>
                 <nav className="row space-between flex-center">
                     <ul>
                         <li>
-                            <Link to="/" className={`btn-clear nav-link btn-hover ${this.state.theme}`}>Battle</Link>
+                            <Link to="/" className={`btn-clear nav-link ${this.state.theme}`}>Battle</Link>
                         </li>
                         <li>
-                            <Link to="/popular" className={`btn-clear nav-link btn-hover ${this.state.theme}`}>Popukar</Link>
+                            <Link to="/popular" className={`btn-clear nav-link ${this.state.theme}`}>Popukar</Link>
                         </li>
                     </ul>
                     <button
                         className="btn-clear nav-link"
                         onClick={toggleTheme}
                     >
-                        {theme === 'light' ? 'Dark Theme' : <div style={{color: "#DADADA"}}>Light Theme</div>}
+                        {theme === 'light' ? '🔦' : '💡'}
                     </button>
-                    <Routes>
-                    <Route exact path="/" element={<div className='container'><Battle /></div>} />
-                    <Route exact path="/popular" element={<div className='container'><Popular /></div>}/>
-                    </Routes>
                 </nav>
-              </Router>
             )}
         </ThemeConsumer>
     )
