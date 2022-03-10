5e9111547dc4ac125b12e200129d8fa6f334fad8
Silvi47 <80204242+Silvi47@users.noreply.github.com>
2022-03-10 20:38:23 +0700

    Update Nav.js

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,34 +1,41 @@
 import React from "react";
 import { ThemeConsumer  } from "../contexts/theme";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Nav () {
     return (
         <ThemeConsumer>
-            {({ theme, toggleTheme }) => (
-                <nav className="row space-between flex-center">
-                    <ul>
-                        <li>
-                            <Link to="/" className={`btn-clear nav-link ${this.state.theme}`}>Battle</Link>
-                        </li>
-                        <li>
-                            <Link to="/popular" className={`btn-clear nav-link ${this.state.theme}`}>Popukar</Link>
-                        </li>
-                    </ul>
-                    <button
-                        className="btn-clear nav-link"
-                        onClick={toggleTheme}
-                    >
-                        {theme === 'light' ? '🔦' : '💡'}
-                    </button>
-                </nav>
-            )}
-        </ThemeConsumer>
+      {({ theme, toggleTheme }) => (
+        <nav className='row space-between'>
+          <ul className='row nav'>
+            <li>
+              <NavLink
+                to='/'
+                exact
+                activeStyle={activeStyle}
+                className='nav-link'>
+                  Popular
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to='/battle'
+                activeStyle={activeStyle}
+                className='nav-link'>
+                  Battle
+              </NavLink>
+            </li>
+          </ul>
+          <button
+            style={{fontSize: 30}}
+            className='btn-clear'
+            onClick={toggleTheme}
+          >
+            {theme === 'light' ? '🔦' : '💡'}
+          </button>
+        </nav>
+      )}
+    </ThemeConsumer>
     )
 }
 
