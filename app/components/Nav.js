690313aec7ad799860470b222b9901fd4b8cbb96
Silvi47 <80204242+Silvi47@users.noreply.github.com>
2022-03-10 23:21:01 +0700

    Update Nav.js

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -14,12 +14,12 @@ function Nav () {
         <nav className='row space-between flex-center'>
           <ul className='row nav'>
             <li>
-              <NavLink to='/' exact style={isActive => ({color: isActive ? "rgb(187, 46, 31)" : "inherit"})} className='nav-link'>
+              <NavLink to='/' exact style={isActive => ({color: isActive ? "rgb(187, 46, 31)" : "black"})} className='nav-link'>
                   Popular
               </NavLink>
             </li>
             <li>
-              <NavLink to='/battle' style={isActive => ({color: isActive ? "rgb(187, 46, 31)" : "inherit"})} className='nav-link'>
+              <NavLink to='/battle' exact style={isActive => ({color: isActive ? "rgb(187, 46, 31)" : "black"})} className='nav-link'>
                   Battle
               </NavLink>
             </li>
