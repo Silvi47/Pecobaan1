77d20fbb8313a23fc39d88261db417312125d0e1
Silvi47 <80204242+Silvi47@users.noreply.github.com>
2022-03-10 23:22:20 +0700

    Update Nav.js

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -19,7 +19,7 @@ function Nav () {
               </NavLink>
             </li>
             <li>
-              <NavLink to='/battle' exact style={isActive => ({color: isActive ? "rgb(187, 46, 31)" : "black"})} className='nav-link'>
+              <NavLink to='/battle' exact style={isActive => ({color: isActive ? "black" : "rgb(187, 46, 31)"})} className='nav-link'>
                   Battle
               </NavLink>
             </li>
