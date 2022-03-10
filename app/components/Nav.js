4f85cb700abebc335a9d6abf86a9f86eea9c4af2
Silvi47 <80204242+Silvi47@users.noreply.github.com>
2022-03-10 20:25:49 +0700

    Update Nav.js

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { ThemeConsumer  } from "../contexts/theme";
+import Popular from './components/Popular'
+import Battle from './components/Battle'
 import {
   BrowserRouter as Router,
   Routes,
