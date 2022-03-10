40ec217ca519413e3d3563924eff5166ab41d48e
Silvi47 <80204242+Silvi47@users.noreply.github.com>
2022-03-10 20:20:06 +0700

    Update Nav.js

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { ThemeConsumer  } from "../contexts/theme";
+import { Link } from "react-router-dom";
 
 function Nav () {
     return (
         <ThemeConsumer>
             {({ theme, toggleTheme }) => (
                 <nav className="row space-between">
+                    <ul>
+                        <li>
+                            <Link to="/" className="nav-link btn-hover">Popular</Link>
+                        </li>
+                        <li>
+                            <Link to="/battle" className="nav-link btn-hover">Battle</Link>
+                        </li>
+                    </ul>
                     <button
                         className="btn-clear nav-link"
                         onClick={toggleTheme}
