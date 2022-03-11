0444350fe8643a2d19b1bbc2700ba7bc4dcba824
Silvi47 <80204242+Silvi47@users.noreply.github.com>
2022-03-11 19:20:59 +0700

    Update theme.js

--- app/contexts/theme.js
+++ app/contexts/theme.js
@@ -2,9 +2,6 @@ import React from "react";
 
 const { Consumer, Provider } = React.createContext()
 
-// Apa yang digunakan untuk consume informasi yang diletakkan di provider
 export const ThemeConsumer = Consumer
-
-// Untuk mengetahui nformasi apa yang available pada komponen apapun yang dibutuhkan aplikasi
 export const ThemeProvider = Provider
 
