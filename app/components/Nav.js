2c7ff2f2f4c454e0e7f91562677e6b697a2a99fb
Silvi47 <80204242+Silvi47@users.noreply.github.com>
2022-03-11 19:29:52 +0700

    Update Nav.js

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -9,31 +9,38 @@ const activeStyle = {
 
 function Nav () {
     return (
-        <ThemeConsumer>
-      {({ theme, toggleTheme }) => (
-        <nav className='row space-between flex-center'>
-          <ul className='row nav'>
-            <li>
-              <NavLink to='/' style={isActive => ({color: isActive ? "rgb(187, 46, 31)" : "black"})} className='nav-link'>
-                  Popular
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to='/battle' style={isActive => ({color: isActive ? "rgb(187, 46, 31)" : "black"})} className='nav-link'>
-                  Battle
-              </NavLink>
-            </li>
-          </ul>
-          <button
-            style={{fontSize: 30}}
-            className='btn-clear'
-            onClick={toggleTheme}
-          >
-            {theme === 'light' ? '🔦' : '💡'}
-          </button>
-        </nav>
-      )}
-    </ThemeConsumer>
+       <ThemeConsumer>
+        {({ theme, toggleTheme }) => (
+          <nav className='row space-between'>
+            <ul className='row nav'>
+              <li>
+                <NavLink
+                  to='/'
+                  exact
+                  activeStyle={activeStyle}
+                  className='nav-link'>
+                    Popular
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to='/battle'
+                  activeStyle={activeStyle}
+                  className='nav-link'>
+                    Battle
+                </NavLink>
+              </li>
+            </ul>
+            <button
+              style={{fontSize: 30}}
+              className='btn-clear'
+              onClick={toggleTheme}
+            >
+              {theme === 'light' ? '🔦' : '💡'}
+            </button>
+          </nav>
+        )}
+      </ThemeConsumer>
     )
 }
 
