0e9d72ffef85be26e21dedab6cd8db35480f6d55
Silvi47 <80204242+Silvi47@users.noreply.github.com>
2022-03-11 23:31:21 +0700

    Update Hover.js

--- app/components/Hover.js
+++ app/components/Hover.js
@@ -1,38 +1,14 @@
 import React from 'react'
 
 export default class Hover extends React.Component {
-    constructor(props) {
-        super(props)
-        
-        this.state = {
-            hovering: false,
-        }
-        
-        this.mouseOver = this.mouseOver.bind(this)
-        this.mouseOut = this.mouseOut.bind(this)
-    }
-    
-    //hovering specific list item
-    mouseOver() {
-        this.setState({
-            hovering: true
-        })
-    }
-    
-    mouseOut() {
-        this.setState({
-            hovering: false
-        })
-    }
-
-    render () {
-        return (
-            <div
-                onMouseOver={this.mouseOver} 
-                onMouseOut={this.mouseOut}
-            >
-                {this.props.children(this.state.hovering)}
-            </div>
-        )
-    }
+  state = { hovering: false }
+  mouseOver = () => this.setState({ hovering: true })
+  mouseOut = () => this.setState({ hovering: false })
+  render () {
+    return (
+      <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut}>
+        {this.props.children(this.state.hovering)}
+      </div>
+    )
+  }
 }
